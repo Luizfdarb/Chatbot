dd959a2e2e1fce65a5f60595223a1cdfbc501f1d
Luizfdarb <78767627+Luizfdarb@users.noreply.github.com>
2023-11-19 21:09:13 -0300

    Update App.js

--- chatbot/src/App.js
+++ chatbot/src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -5,6 +6,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <div className="left-content">
+          <h1>Título Grande</h1>
+          <h2>Seu Subtítulo Aqui</h2>
+        </div>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
